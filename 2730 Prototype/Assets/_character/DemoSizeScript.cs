43a60e75a407b8c5890ef71e4e58e91de6042cd2
ethanrwilson1998 <ethanrwilson1998@gmail.com>
2019-11-03 20:16:57 -0500

    added grow/shrink character demo to project

---
+++ 2730 Prototype/Assets/_character/DemoSizeScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoSizeScript : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+    [Range(0, 1)]
+    [SerializeField] private float size;
+
+    private void Update()
+    {
+        anim.SetFloat("size", size);
+    }
+}
