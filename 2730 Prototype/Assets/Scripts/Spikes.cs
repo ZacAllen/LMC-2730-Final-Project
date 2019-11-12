6428eab12b7c3ce5cda2a54a43d450f49ac2acc1
ethanrwilson1998 <ethanrwilson1998@gmail.com>
2019-11-11 22:23:05 -0500

    merged scenes & cleaned up project organization

---
+++ 2730 Prototype/Assets/Scripts/Spikes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    public GameObject spikes;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = spikes.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.Play("spikes");
+        }
+    }
+}
