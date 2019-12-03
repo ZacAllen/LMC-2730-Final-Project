16da750c14048a34e90614ba17a7eccaed7d2d06
ethanrwilson1998 <43278339+ethanrwilson1998@users.noreply.github.com>
2019-12-03 12:49:28 -0500

    replaced camera rotation to fix a weird nauseous bobbing issue

---
+++ 2730 Prototype/Assets/_character/RotateCamera.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    private Transform tracker;
+    [SerializeField] private Transform lookAt;
+    [SerializeField] private Transform scale;
+
+    private float distance;
+
+    private float vertical;
+    private float horizontal;
+    [SerializeField] private float vScale;
+    [SerializeField] private Vector2 vClamp;
+    [SerializeField] private float hScale;
+
+    private Vector3 velRef;
+    [SerializeField] private float smoothing;
+
+
+    private void Awake()
+    {
+        distance = (transform.position - lookAt.position).magnitude;
+        tracker = new GameObject("camera tracker").transform;
+
+        vertical = vClamp.x + vClamp.y / 2f;
+    }
+
+    private void Update()
+    {
+        tracker.position = lookAt.position - Vector3.forward * distance * scale.localScale.x;
+        tracker.RotateAround(lookAt.position, Vector3.right, vertical);
+
+        tracker.RotateAround(lookAt.position, Vector3.up, horizontal);
+
+        transform.position = Vector3.SmoothDamp(
+            transform.position,
+            tracker.position,
+            ref velRef,
+            smoothing);
+        transform.LookAt(lookAt);
+
+        vertical += Input.GetAxis("Mouse Y") * vScale * Time.deltaTime;
+        vertical = Mathf.Clamp(vertical, vClamp.x, vClamp.y);
+
+        horizontal += Input.GetAxis("Mouse X") * hScale * Time.deltaTime;
+    }
+}
