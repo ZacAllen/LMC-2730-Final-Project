338395b5db8a369172bbe8a7ffcb9c5314cb1a1b
Maddie McRoy <mmcroy@gatech.edu>
2019-12-05 00:08:55 -0500

    menu/UI design

---
+++ 2730 Prototype/Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void LoadLevel4()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+}
