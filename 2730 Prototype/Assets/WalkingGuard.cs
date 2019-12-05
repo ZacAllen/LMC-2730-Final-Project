e4cd857e6c22fd812b0e977ae21db1c7cd5a24ce
ethanrwilson1998 <43278339+ethanrwilson1998@users.noreply.github.com>
2019-12-05 18:16:13 -0500

    updated guards in level 2

---
+++ 2730 Prototype/Assets/WalkingGuard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingGuard : MonoBehaviour
+{
+    [SerializeField] private Transform t;
+    private Vector3 lastPos;
+
+    private void Update()
+    {
+        Vector3 direction = (t.position - lastPos).normalized;
+
+        transform.eulerAngles = new Vector3(0, Vector3.SignedAngle(Vector3.forward, direction, Vector3.up), 0);
+
+        lastPos = t.position;
+    }
+}
