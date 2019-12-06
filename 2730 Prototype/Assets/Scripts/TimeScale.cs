f80d1229859d37584285523ee2cd9f42614064d6
ethanrwilson1998 <ethanrwilson1998@gmail.com>
2019-12-06 01:11:01 -0500

    visual bugfixes

---
+++ 2730 Prototype/Assets/Scripts/TimeScale.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeScale : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+
+}
